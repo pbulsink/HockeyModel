cdb9ba5a03613ec75153386f550c242ed328c902
Philip Bulsink <bulsinkp@gmail.com>
2018-07-24 15:22:48 -0400

    code layout framework

---
+++ R/dixon-coles.R
@@ -0,0 +1,2 @@
+#Hosts dixon-coles specific functions. Many copied from my work from pbulsink.github.io
+# see http://opisthokonta.net/?p=913
---
+++ R/elo.R
@@ -0,0 +1,4 @@
+#Hosts elo specific functions. Many copied from my work from pbulsink.github.io
+#see https://fivethirtyeight.com/features/how-we-calculate-nba-elo-ratings/
+#see https://fivethirtyeight.com/features/introducing-nfl-elo-ratings/
+#see
