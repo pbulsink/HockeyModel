8ebe20da69294723734199cd9c9b4440d984f0a5
Phil OSx <bulsinkp@gmail.com>
2018-07-24 22:12:34 -0400

    Added Data

---
+++ data/data.R
@@ -0,0 +1,25 @@
+#' Historical NHL and WHA scores.
+#'
+#' A dataset containing historical NHL and WHA scores.
+#'
+#' @format A data frame with  variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.hockey-reference.com/}
+"scores"
+
+#' Schedule for 2018-2019.
+#'
+#' A dataframe with the schedule (no scores included) for 2018-2019.
+#'
+#' @format A data frame with  variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.hockey-reference.com/}
+"schedule"
