9da7b7d88302ecf30b3ce888e2623e44149491d3
Philip Bulsink <bulsinkp@gmail.com>
2018-07-25 09:39:51 -0400

    Corrected Data documentation location, content, pass check.

--- R/data.R
+++ R/data.R
@@ -1,17 +1,75 @@
 # Data work. Includes updates of data (daily)
 
+## Datasets Documentation ##
+
+#' Historical NHL and WHA scores.
+#'
+#' A dataset containing historical NHL and WHA scores.
+#'
+#' @format A data frame with  variables:
+#' \describe{
+#'   \item{Date}{Date of the game}
+#'   \item{Visitor}{Name of visiting team}
+#'   \item{VisitorGoals}{Number of goals for the visiting team}
+#'   \item{Home}{Name of the home team}
+#'   \item{HomeGoals}{Number of goals for the home team}
+#'   \item{OTStatus}{The OT status of the game (none, OT, SO, 2OT, 3OT, ...)}
+#'   \item{League}{NHL or WHA}
+#'   \item{Tie}{Boolean, Tie or Not}
+#'   \item{Winner}{Name of the winning team}
+#'   \item{Loser}{Name of the losing team}
+#'   \item{Result}{Coded home win = 0, visitor win = 1}
+#' }
+#' @source \url{http://www.hockey-reference.com/}
+"scores"
+
+#' Schedule for 2018-2019.
+#'
+#' A dataframe with the schedule (no scores included) for 2018-2019.
+#'
+#' @format A data frame with  variables:
+#' \describe{
+#'   \item{Date}{Date of the game played/to be played}
+#'   \item{Visitor}{Name of the visiting team}
+#'   \item{Home}{Name of the home team}
+#'   ...
+#' }
+#' @source \url{http://www.hockey-reference.com/}
+"schedule"
+
+
+#' Update Schedule
+#'
+#' @description Updates the stored schedule if any games are rescheduled due to any event. Adds playoff games as the schedules are released
+#'
+#' @param data_dir The data storage directory
+#' @return True, if successful update or validation, `schedule` is a built in data
+#'
+#' @export
 updateSchedule <- function(data_dir = "./data-raw"){
-  new_schedule<-HockeyScrapR::get_schedule(data_dir = data_dir, from_date=as.Date("2018-10-01"))
+  new_schedule<-HockeyScrapR::getSchedule(data_dir = data_dir, from_date=as.Date("2018-10-01"))
   if(nrow(new_schedule) > nrow(schedule)){
     schedule <- new_schedule
+    schedule <- schedule[,c('Date','Home','Visitor')]
     devtools::use_data(schedule, overwrite = TRUE)
   }
+  return(TRUE)
 }
 
+#' Update Scores
+#'
+#' @description Updates the scores of all finished games.
+#'
+#' @param data_dir The data storage directory
+#' @return True, if successful update or validation, `scores` is a built in data
+#'
+#' @export
 updateScores <- function(data_dir = "./data-raw"){
   new_scores<-HockeyScrapR::updateScores(score_data = scores, data_dir = data_dir)
   if(nrow(new_scores) > nrow(scores)){
     scores<-new_scores
     devtools::use_data(scores, overwrite = TRUE)
   }
+  return(TRUE)
 }
+
