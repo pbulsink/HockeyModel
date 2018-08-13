7336980d6fa1f756ee448cc872e6e40b5bad1bb9
Philip Bulsink <bulsinkp@gmail.com>
2018-08-13 15:13:32 -0400

    Started bradley-terry file

---
+++ R/bradley-terry.R
@@ -0,0 +1,11 @@
+# BradleyTerry2 models W/Ties (GenDavidson)
+# https://www.slideshare.net/htstatistics/turner-use-r2013talk
+# Use Shifted & Scaled Davidson (see ?GenDavidson)
+
+updateBT <- function(){NULL}
+
+plotBT <- function(){NULL}
+
+todayBT <- function(){NULL}
+
+playoffBT <- function(){NULL}
