c66e199265c468b9bda880de98ea13d6b0e0c802
Philip Bulsink <bulsinkp@gmail.com>
2018-10-26 09:56:13 -0400

    cleanup

--- R/multi.R
+++
@@ -1,9 +0,0 @@
-#Combined Predictions - average, or other method
-
-updateMulti <- function(){NULL}
-
-plotMulti <- function(){NULL}
-
-todayMulti <- function(){NULL}
-
-playoffMulti <- function(){NULL}
