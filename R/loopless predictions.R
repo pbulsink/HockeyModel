56dcfce4595e6ecdb339adae14228947ce50791c
Philip Bulsink <bulsinkp@gmail.com>
2018-11-29 15:49:57 -0500

    remainder of changes(forgot to save)

--- R/loopless predictions.R
+++ R/loopless predictions.R
@@ -39,22 +39,9 @@ loopless_sim<-function(nsims=1e4, odds_table = NULL, season_sofar=NULL){
   teams<-as.character(teams[order(teams)])
   n<-length(teams)
 
-  all_results <- data.frame(Team = rep(teams, nsims),
-                            SimNo = rep(1:nsims, each = n),
-                            Points = rep(NA, n * nsims),
-                            W = rep(NA, n*nsims),
-                            L = rep(NA, n * nsims),
-                            OTW = rep(NA, n * nsims),
-                            SOW = rep(NA, n * nsims),
-                            OTL = rep(NA, n * nsims),
-                            SOL = rep(NA, n * nsims),
-                            Rank = rep(NA, n * nsims),
-                            ConfRank = rep(NA, n * nsims),
-                            DivRank = rep(NA, n * nsims),
-                            Playoffs = rep(NA, n * nsims),
-                            stringsAsFactors = FALSE)
 
-  all_results<-lapply_league(multi_season = multi_season, nsims = nsims, all_results = all_results, teams = teams)
+  all_results<-dplyrteam(multi_season = multi_season)
+  all_results$Points<-all_results$W*2 + all_results$OTW*2 + all_results$SOW*2 + all_results$OTL + all_results$SOL
   all_results<-playoffs(all_results=all_results, nsims = nsims)
 
   return(all_results)
@@ -64,15 +51,34 @@ loopless_sim<-function(nsims=1e4, odds_table = NULL, season_sofar=NULL){
 applyteam<-function(multi_season, team, nsims){
   th<-multi_season[multi_season$HomeTeam == team,]
   ta<-multi_season[multi_season$AwayTeam == team,]
-  w<-sapply(1:nsims, function(s) nrow(th[th$Result == 1 & th$sim == s,]) + nrow(ta[ta$Result == 0 & ta$sim == s,]))
-  l<-sapply(1:nsims, function(s) nrow(th[th$Result == 0 & th$sim == s,]) + nrow(ta[ta$Result == 1 & ta$sim == s,]))
-  otw<-sapply(1:nsims, function(s) nrow(th[th$Result == 0.75 & th$sim == s,]) + nrow(ta[ta$Result == 0.25 & ta$sim == s,]))
-  otl<-sapply(1:nsims, function(s) nrow(th[th$Result == 0.25 & th$sim == s,]) + nrow(ta[ta$Result == 0.75 & ta$sim == s,]))
-  sow<-sapply(1:nsims, function(s) nrow(th[th$Result == 0.6 & th$sim == s,]) + nrow(ta[ta$Result == 0.4 & ta$sim == s,]))
-  sol<-sapply(1:nsims, function(s) nrow(th[th$Result == 0.4 & th$sim == s,]) + nrow(ta[ta$Result == 0.6 & ta$sim == s,]))
+  w<-vapply(1:nsims, function(s) nrow(th[th$Result == 1 & th$sim == s,]) + nrow(ta[ta$Result == 0 & ta$sim == s,]), FUN.VALUE = c(0))
+  l<-vapply(1:nsims, function(s) nrow(th[th$Result == 0 & th$sim == s,]) + nrow(ta[ta$Result == 1 & ta$sim == s,]), FUN.VALUE = c(0))
+  otw<-vapply(1:nsims, function(s) nrow(th[th$Result == 0.75 & th$sim == s,]) + nrow(ta[ta$Result == 0.25 & ta$sim == s,]), FUN.VALUE = c(0))
+  otl<-vapply(1:nsims, function(s) nrow(th[th$Result == 0.25 & th$sim == s,]) + nrow(ta[ta$Result == 0.75 & ta$sim == s,]), FUN.VALUE = c(0))
+  sow<-vapply(1:nsims, function(s) nrow(th[th$Result == 0.6 & th$sim == s,]) + nrow(ta[ta$Result == 0.4 & ta$sim == s,]), FUN.VALUE = c(0))
+  sol<-vapply(1:nsims, function(s) nrow(th[th$Result == 0.4 & th$sim == s,]) + nrow(ta[ta$Result == 0.6 & ta$sim == s,]), FUN.VALUE = c(0))
   rm(ta, th)
   return(list(w=w,l=l,otw=otw,otl=otl,sow=sow,sol=sol))
 }
+
+
+
+
+dplyrteam<-function(multi_season){
+  long_season<-data.frame(Team = c(as.character(multi_season$HomeTeam), as.character(multi_season$AwayTeam)), stringsAsFactors = FALSE)
+  long_season$Result<-c(multi_season$Result, 1-multi_season$Result)
+  long_season$SimNo<-c(multi_season$sim, multi_season$sim)
+  all_results<-long_season %>%
+    group_by(SimNo, Team) %>%
+    summarise(W = sum(Result == 1),
+              OTW = sum(Result == 0.75),
+              SOW = sum(Result == 0.6),
+              SOL = sum(Result == 0.4),
+              OTL = sum(Result == 0.25),
+              L = sum(Result == 0))
+  return(as.data.frame(all_results))
+}
+
 
 lapply_league<-function(multi_season, nsims, all_results, teams){
   teamresults<-function(team, multi_season, nsims){
@@ -94,6 +100,7 @@ lapply_league<-function(multi_season, nsims, all_results, teams){
 }
 
 playoffs<-function(all_results, nsims){
+
   for(i in 1:nsims){
     sresult<-all_results[all_results$SimNo == i,]
     sresult$Rank <- rank(-sresult$Points, ties.method = 'random')
@@ -125,5 +132,12 @@ mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
 
 #odds<-remainderSeasonDC(odds = TRUE, regress = TRUE)
 
-#microbenchmark::microbenchmark('old' = HockeyModel:::simulateSeason(odds_table = odds, nsims = 1e2, progress = FALSE),
-                               #'new' = loopless_sim(nsims = 1e2, odds_table = odds))
+mb<-microbenchmark::microbenchmark('old' = HockeyModel:::simulateSeason(odds_table = odds, nsims = 1e2, progress = FALSE),
+                               'new' = loopless_sim(nsims = 1e2, odds_table = odds))
+mb
+ggplot2::autoplot(mb)
+
+t1<-Sys.time()
+sims<-loopless_sim(nsims = 1e3, odds_table = odds_table)
+t2<-Sys.time()
+t2-t1
