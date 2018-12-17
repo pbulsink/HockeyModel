d8b81cf41b67436f8d6ec45d85f58aa859a47dd6
Philip Bulsink <bulsinkp@gmail.com>
2018-12-17 13:22:02 -0500

    Bugfix: Away at Home hashtag for tweet

--- R/main.R
+++ R/main.R
@@ -347,7 +347,7 @@ tweetGames<-function(games = HockeyModel::schedule[HockeyModel::schedule$Date ==
     while(grDevices::dev.cur()!=1){
       grDevices::dev.off()
     }
-    status<-paste0(teamColours[teamColours$Team == home, "Hashtag"], " at ", teamColours[teamColours$Team == away, "Hashtag"], " predicted goals. #HockeyTwitter")
+    status<-paste0(teamColours[teamColours$Team == away, "Hashtag"], " at ", teamColours[teamColours$Team == home, "Hashtag"], " predicted goals. #HockeyTwitter")
     rtweet::post_tweet(status = status,
                        media = file.path(graphic_dir, 'predicted_goals.png'),
                        token = token)
