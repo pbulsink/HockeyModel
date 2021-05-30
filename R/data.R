f6f4601ef01e6be9c45629ffa24ce2163d695444
Philip Bulsink <pbulsink@users.noreply.github.com>
2021-05-30 09:41:50 -0400

    Daily Update 2021-05-30

--- R/data.R
+++ R/data.R
@@ -165,7 +165,7 @@ updateSeries<-function(series = NULL){
   if(is.null(series)){
     series<-data.frame('HomeTeam' = c("Colorado Avalanche", "Vegas Golden Knights", "Toronto Maple Leafs", "Edmonton Oilers", "Pittsburgh Penguins", "Washington Capitals", "Carolina Hurricanes", "Florida Panthers"),
                        'AwayTeam' = c("St. Louis Blues", "Minnesota Wild", "Montreal Canadiens", "Winnipeg Jets", "New York Islanders", "Boston Bruins", "Nashville Predators", "Tampa Bay Lightning"),
-                       'HomeWins' = c(4,3,3,0,2,1,4,2),
+                       'HomeWins' = c(4,4,3,0,2,1,4,2),
                        'AwayWins' = c(0,3,2,4,4,4,2,4),
                        stringsAsFactors = FALSE)
   }
@@ -174,10 +174,10 @@ updateSeries<-function(series = NULL){
 }
 
 updateSeriesNext<-function(series = NULL){
-  series<-data.frame('HomeTeam' = c("Boston Bruins", "Carolina Hurricanes"),
-                     'AwayTeam' = c("New York Islanders", "Tampa Bay Lightning"),
-                     'HomeWins' = c(0,0),
-                     'AwayWins' = c(0,0),
+  series<-data.frame('HomeTeam' = c("Colorado Avalanche", "Boston Bruins", "Carolina Hurricanes"),
+                     'AwayTeam' = c("Vegas Golden Knights", "New York Islanders", "Tampa Bay Lightning"),
+                     'HomeWins' = c(0,0,0),
+                     'AwayWins' = c(0,0,0),
                      stringsAsFactors = FALSE)
 
 }
