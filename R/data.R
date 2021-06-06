1b782decbc663d41b022d94c11f3d03d9463dacb
Philip Bulsink <pbulsink@users.noreply.github.com>
2021-06-06 09:41:13 -0400

    Daily Update 2021-06-06

--- R/data.R
+++ R/data.R
@@ -166,7 +166,7 @@ updateSeries<-function(series = NULL){
     series<-data.frame('HomeTeam' = c("Colorado Avalanche", "Winnipeg Jets", "Boston Bruins", "Carolina Hurricanes"),
                        'AwayTeam' = c("Vegas Golden Knights", "Montreal Canadiens", "New York Islanders", "Tampa Bay Lightning"),
                        'HomeWins' = c(2,0,2,1),
-                       'AwayWins' = c(0,1,1,2),
+                       'AwayWins' = c(2,2,1,2),
                        stringsAsFactors = FALSE)
   }
   usethis::use_data(series, overwrite = TRUE)
