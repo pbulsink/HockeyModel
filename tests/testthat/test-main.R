4eaf1aef2c76354d32582cd49196abfd494935b2
Philip Bulsink <bulsinkp@gmail.com>
2021-09-04 21:18:22 -0400

    Redocumented

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,6 +1,1 @@
-test_that("model Updates OK", {
-  model<-suppressWarnings(updateModel())
-  expect_equal(names(model), c("scores", "schedule", "params"))
-})
-
 #Most of the rest of main just calls the other package functions - testing is of limited benefit
